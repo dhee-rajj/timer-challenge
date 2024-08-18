3f1fb4bda2c9f46d87db021b73c7461a8d0acaa6
dhee-rajj <venkatdheeraj.ind@gmail.com>
2024-08-18 11:08:22 +0530

    for vercel

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
     { targetTime, timeRemaining, onReset },
@@ -6,7 +7,7 @@ const ResultModal = forwardRef(function ResultModal(
 ) {
     const userLost = timeRemaining <= 0;
     const formattedTimeRemaining = (timeRemaining / 1000).toFixed(2);
-    const score = Math.round((1 - (timeRemaining / (targetTime * 1000))) * 100);
+    const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
     const dialog = useRef();
     useImperativeHandle(ref, () => {
         return {
@@ -15,7 +16,7 @@ const ResultModal = forwardRef(function ResultModal(
             },
         };
     });
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal">
             {userLost && <h2>You Lost</h2>}
             {!userLost && <h2>Your Score: {score}</h2>}
@@ -29,7 +30,8 @@ const ResultModal = forwardRef(function ResultModal(
             <form method="dialog" onSubmit={onReset}>
                 <button>close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById("modal")
     );
 });
 
